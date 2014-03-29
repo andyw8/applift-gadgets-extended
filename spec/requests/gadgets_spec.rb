1eb191d2edb8b3ddaf087304e750fa55d6932562
Andy Waite <github.aw@andywaite.com>
2014-03-29 16:11:40 +0100

    Use 'expect' rather than 'should' syntax

--- spec/requests/gadgets_spec.rb
+++ spec/requests/gadgets_spec.rb
@@ -4,7 +4,7 @@ describe "Gadgets" do
   describe "GET /gadgets" do
     it "renders sucessfully" do
       get gadgets_path
-      response.status.should be(200)
+      expect(response.status).to be(200)
     end
   end
 end
