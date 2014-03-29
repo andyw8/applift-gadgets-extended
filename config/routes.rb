dab39695d913b97d71d811e2f90776ea941442c0
Andy Waite <github.aw@andywaite.com>
2014-03-29 18:27:22 +0100

    Remove accidental commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 AppliftGadgets::Application.routes.draw do
-  resources :blahs
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
