790a62eaf5ae878e3673934f4e668a439e091dbc
Andy Waite <github.aw@andywaite.com>
2014-03-29 15:34:23 +0100

    New Rails 4.0.3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AppliftGadgets::Application.config.secret_key_base = '677db8e89248a6ca4c7e37a37668ba75644da0cde5b60e2dff96471462d5f15b3fac424d0605a3dbc55ad1c631276135ad3ee725e3630843336c92a2cec78b09'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AppliftGadgets::Application.config.session_store :cookie_store, key: '_applift-gadgets_session'
