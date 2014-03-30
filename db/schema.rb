4bd828ba3a6b629455a92dc689387b296dbe7508
Andy Waite <github.aw@andywaite.com>
2014-03-30 11:11:20 +0200

    Allow attachments on gadget model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140329181201) do
+ActiveRecord::Schema.define(version: 20140330113337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140329181201) do
     t.datetime "updated_at"
     t.string   "name"
     t.integer  "owner_id"
+    t.string   "image"
   end
 
   create_table "users", force: true do |t|
